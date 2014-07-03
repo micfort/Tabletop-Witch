c22bf6e72b322e6406db929626b56e2112c7774e
micfort <michiel.fortuin@gmail.com>
2014-07-03 22:06:21 +0200

    Added basic implementation of index.html with cloudant Added library's chui and jquery

--- GMDroid/www/js/index.js
+++ GMDroid/www/js/index.js
@@ -1,49 +1,86 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
-var app = {
-    // Application Constructor
-    initialize: function() {
-        this.bindEvents();
-    },
-    // Bind Event Listeners
-    //
-    // Bind any events that are required on startup. Common events are:
-    // 'load', 'deviceready', 'offline', and 'online'.
-    bindEvents: function() {
-        document.addEventListener('deviceready', this.onDeviceReady, false);
-    },
-    // deviceready Event Handler
-    //
-    // The scope of 'this' is the event. In order to call the 'receivedEvent'
-    // function, we must explicitly call 'app.receivedEvent(...);'
-    onDeviceReady: function() {
-        app.receivedEvent('deviceready');
-    },
-    // Update DOM on a Received Event
-    receivedEvent: function(id) {
-        var parentElement = document.getElementById(id);
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
+Parse.initialize("14Q2hQn42Q77RxuEb19PghEVKWPfsr6UdSJCKxjc", "qpK5SxyggMmZqQ8tqNM60TRQPDaHVrMlzBdG0lUk");
 
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
+if (typeof cordova !== "undefined")
+{
+	var deviceReadyDeferred = $.Deferred();
+	var jqmReadyDeferred = $.Deferred();
 
-        console.log('Received Event: ' + id);
-    }
-};
+	document.addEventListener("deviceReady", deviceReady, false);
+
+	function deviceReady()
+	{
+		deviceReadyDeferred.resolve();
+	}
+
+	$(document).ready(function ()
+	{
+		jqmReadyDeferred.resolve();
+	});
+
+	$.when(deviceReadyDeferred, jqmReadyDeferred).then(deviceAndDomReady);
+}
+else
+{
+	$(document).ready(function ()
+	{
+		deviceAndDomReady();
+	});
+}
+
+
+function deviceAndDomReady()
+{
+	$(".editButton").on( "click", function()
+	{
+		var TestObject = Parse.Object.extend("TestObject");
+		var testObject = new TestObject();
+		testObject.save({foo: "bar"}).then(function(object) {
+			alert("yay! it worked");
+		});
+	});
+
+	$.ajax({
+		url: "https://micfort.cloudant.com/micfort_gmdroid_pf_monsters_r1/_design/names/_view/value",
+		dataType: "jsonp",
+		crossDomain: true,
+		success: LoadList
+	});
+}
+
+function LoadList(list)
+{
+	var HTML = "";
+	var count = list.rows.length;
+	list.rows.sort(
+		function compare(a, b)
+		{
+			if (a.value < b.value)
+				return -1;
+			if (a.value > b.value)
+				return 1;
+			return 0;
+		});
+
+	for (var i = 0; i < count; i++)
+	{
+		HTML += "<li data-goto=\"monster\" class=\"nav monsterItem\" data-monsterid=\"" + list.rows[i].id + "\"><h3>" + list.rows[i].value + "</h3></li>";
+	}
+	$("#monsterList").html(HTML);
+	$(".monsterItem").on( "click", function()
+	{
+		$("#monster").html("<section>loading</section>");
+		$.ajax({
+			url: "https://micfort.cloudant.com/micfort_gmdroid_pf_monsters_r1/"+$(this).data("monsterid"),
+			dataType: "jsonp",
+			crossDomain: true,
+			success: loadMonster
+		});
+	});
+}
+
+function loadMonster(monster)
+{
+	$("#monster").html("<section>" + monster.html + "</section>");
+}
+
+document.addEventListener('deviceready', this.onDeviceReady, false);
