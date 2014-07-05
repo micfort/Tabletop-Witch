4dc9b4ae9d4ce54bcf56deb9c9cf3c94c8de68ab
micfort <michiel.fortuin@gmail.com>
2014-07-05 17:19:27 +0200

    Further porting multipage system

--- Website/js/sets.js
+++ Website/js/sets.js
@@ -1,33 +1,29 @@
 /**
  * Created by s120397 on 4-7-14.
  */
-LoadPageData["sets.html"] =
-{
-	navbarItem: $("li.link[data-goto='sets.html']"),
-	visibilityState: {"loggedIn": true, "loggedOut": false},
-	ready: function()
-	{
-		var currentUser = Parse.User.current();
-		$("#sets-username").text(currentUser.get("username"));
 
-		var Set = Parse.Object.extend("Set");
-		var SetQuery = new Parse.Query(Set);
-		var userID = currentUser.id;
-		SetQuery.equalTo("owner",currentUser);
-		SetQuery.find({
-			success:function(results)
+$(document).on("sessionReady", function (e)
+{
+	var currentUser = Parse.User.current();
+	$("#sets-username").text(currentUser.get("username"));
+
+	var Set = Parse.Object.extend("Set");
+	var SetQuery = new Parse.Query(Set);
+	var userID = currentUser.id;
+	SetQuery.equalTo("owner",currentUser);
+	SetQuery.find({
+		success:function(results)
+		{
+			var html = "";
+			for(var i = 0; i < results.length; i++)
 			{
-				var html = "";
-				for(var i = 0; i < results.length; i++)
-				{
-					html = html + "<p>" + results[i].get("name") + "</p>";
-				}
-				$("#sets-list").html(html);
-			},
-			error: function(error)
-			{
-				alert("Error: " + error.code + " " + error.message);
+				html = html + "<p>" + results[i].get("name") + "</p>";
 			}
-		})
-	}
-}
+			$("#sets-list").html(html);
+		},
+		error: function(error)
+		{
+			alert("Error: " + error.code + " " + error.message);
+		}
+	});
+});
